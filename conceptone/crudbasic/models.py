bee0fb8f34cbfcf8a36762fad9e35f18354aab82
asifmanan <asifmanan@gmail.com>
2019-09-28 00:47:52 +0500

    Added created and updated date in models, started working on homepage tiles

--- conceptone/crudbasic/models.py
+++ conceptone/crudbasic/models.py
@@ -10,16 +10,25 @@ class Customers(models.Model):
     customer_fax = models.CharField(max_length=128, blank=True)
     customer_email = models.EmailField(max_length=192, blank=True)
     customer_ntn_number = models.CharField(max_length=128, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.customer_name
 
 class Projects(models.Model):
+    status_flags = [
+        ('Prospective','Prospective'),
+        ('Completed','Completed'),
+        ('On-Going','On-Going'),
+    ]
     customer = models.ForeignKey(Customers, on_delete=models.CASCADE)
     project_id = models.CharField(max_length=16, unique=True)
     project_name = models.CharField(max_length=128, unique=True)
     project_city = models.CharField(max_length=128)
-    project_worth = models.DecimalField(max_digits=10, decimal_places=2,default=0.00, blank=True)
+    project_status = models.CharField(max_length=16,choices=status_flags)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.project_name
@@ -33,6 +42,8 @@ class Items(models.Model):
     item_description = models.CharField(max_length=192)
     item_uom = models.CharField(max_length=16)
     item_type = models.CharField(max_length=8, choices=item_type_choices)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.item_description
