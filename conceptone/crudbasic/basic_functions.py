e20e32e519c4faef3291cb7a75e9bc29147be425
asifmanan <asifmanan@gmail.com>
2020-04-15 16:45:59 +0500

    PO Print 4

--- conceptone/crudbasic/basic_functions.py
+++ conceptone/crudbasic/basic_functions.py
@@ -2,12 +2,13 @@ import io
 
 from reportlab.pdfgen import canvas
 
+from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter, A4
 from reportlab.lib.units import inch, cm
 from reportlab.lib.styles import getSampleStyleSheet
 
-from reportlab.platypus import doctemplate, Paragraph, Frame
-from reportlab.platypus.tables import Table
+from reportlab.platypus import doctemplate, Paragraph, Frame, BaseDocTemplate, SimpleDocTemplate
+from reportlab.platypus.tables import Table, TableStyle
 
 from reportlab.rl_config import defaultPageSize
 
@@ -33,35 +34,57 @@ def generatePdf(po_obj,po_lines):
     aW = PAGE_WIDTH
     aH = PAGE_HEIGHT
     w,h = p.wrap(aW,aH)
-    print(w,h)
+    # print(w,h)
 
     # story.append(Paragraph("This is a Heading",styleH))
     # story.append(Paragraph("This is a Paragraph in Normal Style",styleN))
     line_description = Paragraph("Excavation of trench, pipe laying and backfilling in normal soil as per specifications",styleN)
-
+    # print(line_description)
     data = [["Line","Description","Quantity","UoM","Unit Price","Total"],[1,line_description,"100","KM","155","15500"]]
-    t=Table(data,(2.0*cm,6*cm,2.5*cm,2*cm,3*cm,3*cm))
+    tData = [["Line","Description","Quantity","UoM","Unit Price","Total"],]
+
+    for item in po_lines:
+        line_description = Paragraph(item.order_item.item_description,styleN)
+        # print(line_description)
+        tData.append([item.po_line_number,line_description,
+                        item.order_quantity,item.order_item.item_uom,
+                        item.purchase_price,item.total_price])
+
+    t=Table(tData,(2.0*cm,6*cm,2.5*cm,2*cm,3*cm,3*cm))
     w,h = t.wrap(aW,aH)
-    print(w,h)
+    print(w/cm,h/cm)
 
     elements.append(t)
 
     # buyerName = Paragraph()
+    infoTableStyle = TableStyle(
+                    [('LINEABOVE', (0,0), (-1,0), 1, colors.green),
+                    ('LINEABOVE', (0,1), (-1,-1), 0.25, colors.black),
+                    ('LINEBELOW', (0,-1), (-1,-1), 1, colors.green),
+                    ('ALIGN', (1,1), (-1,-1), 'LEFT')]
+                    )
+
     buyerInfo = [["PO Number",po_obj.po_number],["PO Date",po_obj.po_date]]
     supplierInfo = [["Supplier Name",po_obj.po_supplier.supplier_name],
-                    ["Supplier Address",po_obj.po_supplier.supplier_address],
-                    ["Supplier NTN",po_obj.po_supplier.supplier_ntn_number]
+                    ["Supplier Address",po_obj.po_supplier.supplier_address+", "+po_obj.po_supplier.supplier_city],
+                    ["Supplier NTN",po_obj.po_supplier.supplier_ntn_number],
+                    ["Supplier Phone",po_obj.po_supplier.supplier_phone],
+                    ["supplier_email",po_obj.po_supplier.supplier_email],
                     ]
 
-    infoTable = Table(supplierInfo,(3.0*cm,6*cm))
+
+
+    infoTable = Table(supplierInfo,(3.0*cm,6*cm),style=infoTableStyle)
+    infoTable.hAlign = 'LEFT'
     story.append(infoTable)
 
-    f1 = Frame(1*cm,23.7*cm,19*cm,5*cm,showBoundary=1)
-    f2 = Frame(1*cm,17.7*cm,19*cm,5.5*cm,showBoundary=1)
+    f1 = Frame(1*cm,23.7*cm,19*cm,5*cm,showBoundary=0)
+    f2 = Frame(1*cm,10.7*cm,19*cm,12.5*cm,showBoundary=1)
     f1.addFromList(story,c)
     f2.addFromList(elements,c)
     c.save()
     buffer.seek(0)
+    # print(tData)
     return buffer
 
 def printpo2pdf(po_obj, po_lines):
@@ -107,6 +130,63 @@ def printpo2pdf(po_obj, po_lines):
     p.save()
     buffer.seek(0)
     return buffer
+
+class Print_PO(BaseDocTemplate):
+    def __init__(self, filename, their_adress, objects, **kwargs):
+        super().__init__(filename, page_size=A4, _pageBreakQuick=0, **kwargs)
+        self.their_adress = their_adress
+        self.objects = objects
+
+        self.page_width = (self.width + self.leftMargin * 2)
+        self.page_height = (self.height + self.bottomMargin * 2)
+
+
+        styles = getSampleStyleSheet()
+
+        # Setting up the frames, frames are use for dynamic content not fixed page elements
+        first_page_table_frame = Frame(self.leftMargin, self.bottomMargin, self.width, self.height - 6 * cm, id='small_table')
+        later_pages_table_frame = Frame(self.leftMargin, self.bottomMargin, self.width, self.height, id='large_table')
+
+        # Creating the page templates
+        first_page = PageTemplate(id='FirstPage', frames=[first_page_table_frame], onPage=self.on_first_page)
+        later_pages = PageTemplate(id='LaterPages', frames=[later_pages_table_frame], onPage=self.add_default_info)
+        self.addPageTemplates([first_page, later_pages])
+
+        # Tell Reportlab to use the other template on the later pages,
+        # by the default the first template that was added is used for the first page.
+        story = [NextPageTemplate(['*', 'LaterPages'])]
+
+        table_grid = [["Product", "Quantity"]]
+        # Add the objects
+        for shipped_object in self.objects:
+            table_grid.append([shipped_object, "42"])
+
+        story.append(Table(table_grid, repeatRows=1, colWidths=[0.5 * self.width, 0.5 * self.width],
+                           style=TableStyle([('GRID',(0,1),(-1,-1),0.25,colors.gray),
+                                             ('BOX', (0,0), (-1,-1), 1.0, colors.black),
+                                             ('BOX', (0,0), (1,0), 1.0, colors.black),
+                                             ])))
+
+        self.build(story)
+
+    def on_first_page(self, canvas, doc):
+        canvas.saveState()
+        # Add the logo and other default stuff
+        self.add_default_info(canvas, doc)
+
+        canvas.drawString(doc.leftMargin, doc.height, "My address")
+        canvas.drawString(0.5 * doc.page_width, doc.height, self.their_adress)
+
+        canvas.restoreState()
+
+    def add_default_info(self, canvas, doc):
+        canvas.saveState()
+        canvas.drawCentredString(0.5 * (doc.page_width), doc.page_height - 2.5 * cm, "Company Name")
+
+        canvas.restoreState()
+
+# if __name__ == '__main__':
+#     ShippingListReport('example.pdf', "Their address", ["Product", "Product"] * 50)
 
 def get_col_heads(caller):
     caller_fields = caller._meta.fields
