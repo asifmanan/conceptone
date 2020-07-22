f90839af36e554a6f9d6b4c4923295912370fa60
asifmanan <asifmanan@gmail.com>
2020-07-22 10:34:30 +0500

    saleorderApp fixed, salesApp removed

--- conceptone/conceptone/settings.py
+++ conceptone/conceptone/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crudbasic',
     'baseApp',
-    'salesApp',
     'standardinvoiceApp',
     'saleordersApp',
     'purchaseApp',
--- conceptone/salesApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SalesappConfig(AppConfig):
-    name = 'salesApp'
