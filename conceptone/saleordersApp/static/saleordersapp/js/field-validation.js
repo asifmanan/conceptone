599c0d7a148f3dbf5a38dae00a9f40a8eb7796f3
asifmanan <asifmanan@gmail.com>
2020-07-22 20:58:18 +0500

    working on js of createsaleorderitem

--- conceptone/saleordersApp/static/saleordersapp/js/field-validation.js
+++ conceptone/saleordersApp/static/saleordersapp/js/field-validation.js
@@ -1,11 +1,24 @@
-$("#id_order_quantity").change(function () {
-  var order_quantity = $("#id_order_quantity").val();
-  console.log(order_quantity);
-  if(isNumeric($("#id_order_quantity").val())) {
-    console.log("value is good");
-    // $("#order_quantity").val("good")
-  }
-  else{
-    console.log("Not Numeric");
-  }
+$(document).ready(function(){
+    console.log("Document is ready");
+    $('#id_order_quantity').change(input_validation ()
+      // var item = $('#id_order_quantity').val();
+      // console.log("its working!");
+      // if($().isNumeric($("#id_order_quantity").val())) {
+      //   console.log("value is good");
+      //   $("#order_quantity").val("good")
+      // }
+      // else{
+      //   console.log("Not Numeric");
+      // }
+    );
 });
+$("#id_order_quantity").change(input_validation());
+$("#id_sale_price").change(input_validation());
+
+function input_validation () {
+  var order_quantity = $("#id_order_quantity").val();
+  var sale_price = $("#id_sale_price").val()
+  amount = order_quantity*sale_price
+  $("#id_amount").val(amount);
+  console.log(amount);
+}
