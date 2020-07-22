4809733fe82b9f0c8589ebf6e84393e118dbfa7b
asifmanan <asifmanan@gmail.com>
2020-07-22 23:15:45 +0500

    changes in saleordersApp-js

--- conceptone/saleordersApp/static/saleordersapp/js/field-validation.js
+++ conceptone/saleordersApp/static/saleordersapp/js/field-validation.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     console.log("Document is ready");
-    $('#id_order_quantity').change(input_validation ()
+    $("#id_amount").val("900000");
+    // $("#id_order_quantity").change(input_validation());
+    // $("#id_sale_price").change(input_validation());
       // var item = $('#id_order_quantity').val();
       // console.log("its working!");
       // if($().isNumeric($("#id_order_quantity").val())) {
@@ -12,13 +14,13 @@ $(document).ready(function(){
       // }
     );
 });
-$("#id_order_quantity").change(input_validation());
-$("#id_sale_price").change(input_validation());
+// $("#id_order_quantity").change(input_validation());
+// $("#id_sale_price").change(input_validation());
 
 function input_validation () {
   var order_quantity = $("#id_order_quantity").val();
   var sale_price = $("#id_sale_price").val()
   amount = order_quantity*sale_price
-  $("#id_amount").val(amount);
+  $("#id_amount").innerHTML(amount);
   console.log(amount);
 }
