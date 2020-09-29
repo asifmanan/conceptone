ae9bc056b14f5671b46bb054f2492f90750b8e99
asifmanan <asifmanan@gmail.com>
2020-09-29 19:59:37 +0500

    saleorderinvoice template in progress

--- conceptone/baseApp/static/baseapp/js/sidebar-menu.js
+++ conceptone/baseApp/static/baseapp/js/sidebar-menu.js
@@ -2,3 +2,8 @@ $("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
+
+$("#id--toggle-table-view").click(function(e) {
+    e.preventDefault();
+    $(".collapse--field").toggleClass("compressed");
+});
