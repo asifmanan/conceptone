97bb03741d61ba2805e263b9e99161e61005feb0
asifmanan <asifmanan@gmail.com>
2020-10-05 17:58:17 +0500

    sidebar menu changes

--- conceptone/baseApp/static/baseapp/js/sidebar-menu.js
+++ conceptone/baseApp/static/baseapp/js/sidebar-menu.js
@@ -3,6 +3,20 @@ $("#menu-toggle").click(function(e) {
     $("#wrapper").toggleClass("toggled");
 });
 
+$(".sidenav-btn-collapse").click(function(e) {
+   e.preventDefault();
+    if (!$(this).next(".sidebar-collapse-menu").hasClass("show")) {
+        console.log("its open");
+        $(".sidebar-collapse-menu").each(function(){
+          $(".sidebar-collapse-menu").removeClass("show");
+        })
+        $(this).next(".sidebar-collapse-menu").addClass("show");
+    }
+    else {
+        $(this).next(".sidebar-collapse-menu").removeClass("show");
+    }
+});
+
 $("#id--toggle-table-view").click(function(e) {
     e.preventDefault();
     $(".collapse--field").toggleClass("compressed");
